187b7395d7cd79c26fc117baa94e3804cad0fb15
jlinares <jorgelinaresrrs301@gmail.com>
2025-07-09 23:48:00 -0500

    cache

---
+++ KidycareBackend/Reviews/Interfaces/Resources/UpdateReviewResource.cs
@@ -0,0 +1,7 @@
+﻿namespace KidycareBackend.Reviews.Interfaces.Resources;
+
+public record UpdateReviewResource(
+    int rating,
+    string comment,
+    DateTime date
+    );
---
+++ KidycareBackend/Reviews/Interfaces/Transform/UpdateReviewByIdCommandFromResourceAssembler.cs
@@ -0,0 +1,19 @@
+﻿using KidycareBackend.Reviews.Domain.Model.Commands;
+using KidycareBackend.Reviews.Interfaces.Resources;
+
+namespace KidycareBackend.Reviews.Interfaces.Transform;
+
+public class UpdateReviewByIdCommandFromResourceAssembler
+{
+    public static UpdateReviewByIdCommand ToCommandFromResource(
+        UpdateReviewResource resource, int Id)
+    {
+        return new UpdateReviewByIdCommand(
+            resource.rating,
+            resource.comment,
+            resource.date
+        );
+        
+    }
+
+}
